9a56aad297d835f93af7dcbd4f54099b8762f1c3
Elizabeth <kenkwoelizabeth78@gmail.com>
2023-09-21 22:47:59 -0400

    Adding Arrays

---
+++ src/main/java/org/example/Array.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers={3,7,5,-5};
+        for (int number: numbers){
+            System.out.println(number);
+        }
+        int[] numbers1={3,7,5,-5,9,20};
+        int size= numbers1.length;
+        for(int i=0; i < size;i++){
+            System.out.println(i);
+        }
+    }
+}
