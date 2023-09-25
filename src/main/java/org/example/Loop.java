d93b93006c8975cc7a63e086796a15e1aa697ee8
Elizabeth <kenkwoelizabeth78@gmail.com>
2023-09-25 09:29:19 -0400

    Corrected Loop

--- src/main/java/org/example/Loop.java
+++ src/main/java/org/example/Loop.java
@@ -7,25 +7,25 @@ public class Loop {
         int m = 99;
         // A loop that will display a string 99 times.
         for (int i = 0; i < m; i++) {
-                //    System.out.println("Software Developer"+ i);
+                    System.out.println("Software Developer"+ i);
         }
         //A loop that will display every odd number from 0 to 99
         for (int i = 0; i <= m; i++) {
             if (i % 2 == 1) {
-                //          System.out.println(i);
+                          System.out.println(i);
             }
         }
         // A loop that will display every even number from 0 to 99
 
         for (int i = 0; i <= m; i++) {
             if (i % 2 == 0) {
-                //          System.out.println(i);
+                System.out.println(i);
             }
         }
         // The same loop as 3, but in a different way.
         for (int i = 0; i < m; i++) {
             while (i % 2 == 0) {
-                //         System.out.println(i);
+                        System.out.println(i);
                 i++;
             }
         }
@@ -58,21 +58,32 @@ public class Loop {
         //A loop that finds a sum of the numbers
         // between two numbers that are given by the user, inclusive.
         //To receive user input
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
-        //output for a user to reply
-        System.out.println("Enter First Number:");
+        // Input two numbers
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
 
-        // Returns a line of text that is read from scanner
-        int num1 = sc.nextInt();
-        System.out.println("Enter Second Number:");
-        int num2 = sc.nextInt();
+        // Initialize a variable to store the sum
         int sum = 0;
-        // for loop for the problem
-        for (int i = num1+1; i <num2; i++) {
+
+        // Ensure that num1 is smaller or equal to num2
+        if (num1 > num2) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
+
+        // Loop to find the sum of numbers between num1 and num2 (inclusive)
+        for (int i = num1; i <= num2; i++) {
             sum += i;
         }
-        System.out.println("Sum is " + sum);
-    }
 
+        // Display the result
+        System.out.println("The sum of numbers between " + num1 + " and " + num2 + " (inclusive) is: " + sum);
+    }
 }
+
+
